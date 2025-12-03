f16708f53a4474647b17eb2f76a070c333262427
MichaelKarabinosh <michaelk368@nycstudents.net>
2025-12-03 00:55:57 -0500

    D3 P1 AND P2, brute forced P1 bc I was too lazy, then part 2 made me think of a pretty elegant solution

---
+++ Solutions/AOC2025D3.py
@@ -0,0 +1,47 @@
+with open('../Inputs/InputFile3', 'r') as file:
+    lines = file.readlines()
+newlines = []
+for line in lines:
+    line = line.strip('\n')
+    newlines.append(line)
+
+def find_max(line):
+    return "".join(sorted(line,reverse=True))[0]
+
+
+def hyp_iterate(line):
+    max = 0
+    for i in range(len(line) - 1):
+        num = int(line[i] + find_max(line[i+1:]))
+        if num > max:
+            max = num
+    return max
+
+def part_one():
+    total = 0
+    for coltage in newlines:
+        total += hyp_iterate(coltage)
+    return total
+
+def part_two():
+    total = 0
+    for colage in newlines:
+        total += int(hyp_iterate2(colage))
+    return total
+
+def hyp_iterate2(line):
+    numbers_left = 12
+    index = 0
+    str = ""
+    length= len(line)
+    while numbers_left > 0:
+        # print(line[index:(15-numbers_left) + 1])
+        max = find_max(line[index:(length-numbers_left) + 1])
+        str += max
+        index = line[index:(length-numbers_left) + 1].index(max) + index + 1
+        # print(index)
+        numbers_left -= 1
+    return str
+
+print('Part One:', part_one())
+print('Part Two:', part_two())
