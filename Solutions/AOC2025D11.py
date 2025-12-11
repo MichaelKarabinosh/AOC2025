d8c4863a0118c769897973e2901d8af274ee3863
MichaelKarabinosh <michaelk368@nycstudents.net>
2025-12-11 10:20:16 -0500

    D10p2 progress

--- Solutions/AOC2025D11.py
+++ Solutions/AOC2025D11.py
@@ -1,5 +1,5 @@
 import networkx
-
+from matplotlib import pyplot as plt
 
 with open('../Inputs/InputFile11', 'r') as file:
     lines = file.readlines()
@@ -8,7 +8,8 @@ for line in lines:
     line = line.strip('\n')
     newlines.append(line)
 
-network = networkx.DiGraph() # directed graph means nodes are one way
+network = networkx.DiGraph()# directed graph means nodes are one way
+
 dict = {}
 for line in newlines:
     split = line.split(':')
@@ -16,7 +17,7 @@ for line in newlines:
     placeholder.pop(0) # include falsy value added
     dict[split[0]] = placeholder
     for item in placeholder:
-        network.add_edge(split[0], item) # add all edges
+        network.add_edge(split[0], item)
 
 def part_one(): # lol thanks networkx
     part_one_answer = 0
@@ -26,11 +27,24 @@ def part_one(): # lol thanks networkx
     return part_one_answer
 
 def part_two():
-    part_two_answer = 0
-    for path in networkx.all_shortest_paths(network, source='svr', target='out'):
-        if 'dac' in path and 'fft' in path:
-            part_two_answer += 1
-    return part_two_answer
+    sort_order = list(networkx.topological_sort(network))
+    path_counts = {node: 0 for node in network.nodes()}
+    source = 'svr'
+    destination = 'dac'
+    path_counts[source] = 1
+
+    # Iterate through nodes in topological order
+    for node in sort_order:
+        # If the destination is reached, we can stop the overall count if we only care about it
+        if node == destination:
+            break
+
+        # Update counts for neighbors
+        if path_counts[node] > 0:
+            for successor in network.successors(node):
+                path_counts[successor] += path_counts[node]
+
+    return path_counts[destination]
 
 
 # print('Part One:', part_one())
